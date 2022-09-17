20118f97e6afb3df6d5f8729b23a682a182fa995
aaryadevg <aaryadevg@gmail.com>
2022-09-17 13:53:22 +0200

    changed appsettings and created base repository

---
+++ ConnectedOffice/DeviceManagement_WebApp/Respoistory/IBaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeviceManagement_WebApp.Respoistory
+{
+	public interface IBaseRepository<T> where T : class
+	{
+		T GetById(Guid id);
+		IEnumerable<T> GetAll();
+		void Add(T entity);
+		void Update(T entity);
+		void Delete(Guid id);
+	}
+}
